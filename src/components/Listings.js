f70e52e228e58b06e8f0c8964e4f5a90891c75f8
peterkrumb <petekrumb@gmail.com>
2020-03-15 00:59:58 -0500

    more styling to content area

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -9,21 +9,77 @@ class Listings extends Component {
         }
     }
     render() {
-  return (<section className='listings'>
+  return (<section id='listings'>
       <section className='search-area'>
         <input type='text' name='search'></input>
       </section>
 
-      <section className='sortBy'>
-
+      <section className='sortby-area'>
+      <div className='results'>390 results found</div>
+      <div className='sort-options'>
+        <select name='sortby' className='sortby'>
+          <option value='price-asc'>Highest Price</option>
+          <option value='price-dsc'>Lowest Price</option>
+        </select>
+        <div className='view'>
+        <i className="fas fa-list-ul"></i>
+        <i class="fas fa-th"></i>
+        </div>
+      </div>
       </section>
 
         <section className='listings-results'>
+          <div className='listing'>
+            <div className='listing-image'>
+              <span className='address'>23 Dexi Hills</span>
+              <div className='details'>
+                <div className='col-md-3'>
+                <div className='user-img'></div>
+                </div>
 
+                <div className='col-md-9'>
+                <div className='user-details'>
+                  <span className='user-name'>Nina Smith</span>
+                  <span className='post-date'>05/08/2019</span>
+                </div>
+                <div className='listing-details'>
+                  <div className='floor-space'>
+                    <i className="far fa-square"></i>
+                  <span>1000 ft&sup2;</span>
+                  </div>
+                  <div className='bedrooms'>
+                    <i className="fas fa-bed"></i>
+                    <span>3 bedrooms</span>
+                  </div>
+                </div>
+
+                <div className='view-btn'>
+                  View Listing
+                </div>
+
+                </div>
+                
+              </div>
+            </div>
+              <div className='bottom-info'>
+                <span className='price'>$1000 / month</span>
+                <span className='location'><i className="fas fa-map-marker-alt">
+                </i>Richwood, NY</span>
+                  
+              </div>
+          </div>
         </section>
 
         <section className='pagination'>
-
+          <ul className='pagination-nums'>
+            <li>Prev</li>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>Next</li>
+          </ul>
         </section>
 
   </section>
